882a3e196d0182e6eddd3c24c19b8d39325f11ce
Guilherme Cristaldo <guilherme.cristaldo15@gmail.com>
2026-01-26 21:47:56 -0300

    feat: add main menu loop using ReadKey

---
+++ MeatControl.Console/MeatControl.Console/Meat.cs
@@ -0,0 +1,10 @@
+﻿namespace MeatControl.Console
+{
+    internal class Meat
+    {
+        public int Id { get; private set; }
+        public string Cut { get; set; }
+        public double Price { get; set; }
+
+    }
+}
--- MeatControl.Console/MeatControl.Console/Program.cs
+++ MeatControl.Console/MeatControl.Console/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+char option;
+
+do
+{
+    Console.WriteLine("1 - Add meat");
+    Console.WriteLine("2 - Remove meat");
+    Console.WriteLine("3 - List all meats");
+    Console.WriteLine("4 - Edit meat");
+    Console.WriteLine("0 - Exit");
+
+
+    ConsoleKeyInfo key = Console.ReadKey(true);
+    option = key.KeyChar;
+}
+while (option != '0');
+
