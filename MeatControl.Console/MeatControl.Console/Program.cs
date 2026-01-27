511550752b2a9df001c669156ff37b09a9353577
Guilherme Cristaldo <guilherme.cristaldo15@gmail.com>
2026-01-26 21:58:20 -0300

    feat: add menu routing with switch case

--- MeatControl.Console/MeatControl.Console/Program.cs
+++ MeatControl.Console/MeatControl.Console/Program.cs
@@ -9,9 +9,29 @@ do
     Console.WriteLine("4 - Edit meat");
     Console.WriteLine("0 - Exit");
 
+    Console.Clear();
 
     ConsoleKeyInfo key = Console.ReadKey(true);
     option = key.KeyChar;
+
+    switch (option)
+    {
+        case '1':
+            Console.WriteLine("Adding meat...");
+            break;
+        case '2':
+            Console.WriteLine("Removing meat...");
+            break;
+        case '3':
+            Console.WriteLine("Listing all meats...");
+            break;
+        case '4':
+            Console.WriteLine("Editing meat...");
+            break;
+    default:
+            Console.WriteLine("Invalid option!");
+            break;
+    }
 }
 while (option != '0');
 
