d77c529d8d0e367774f234a40bb90363626085cf
Guilherme Cristaldo <guilherme.cristaldo15@gmail.com>
2026-01-29 21:50:01 -0300

    feat: implement edit meat and complete CRUD flow

--- MeatControl.Console/MeatControl.Console/Program.cs
+++ MeatControl.Console/MeatControl.Console/Program.cs
@@ -34,11 +34,11 @@ internal class Program
                     PressKeyMessage();
                     break;
                 case '4':
-                    Console.WriteLine("Editing meat...");
+                    EditMeat(filePath);
                     PressKeyMessage();
                     break;
                 case '0':
-                    Console.WriteLine("Exiting...");
+                    Environment.Exit(0);
                     break;
                 default:
                     Console.WriteLine("Invalid option!");
@@ -47,15 +47,213 @@ internal class Program
             }
         }
         while (option != '0');
+    }
 
-        static void AddMeat(string filePath)
+    static void AddMeat(string filePath)
+    {
+        bool continueOption;
+
+        do
         {
-            bool continueOption;
+            Console.Clear();
+
+            Console.Write("Type of meat: ");
+            string type = Console.ReadLine();
+
+            double price;
+            bool valid;
+            do
+            {
+                Console.Write("Price: ");
+                string input = Console.ReadLine();
+
+                valid = double.TryParse(input, NumberStyles.Any, CultureInfo.InvariantCulture, out price);
+
+                if (!valid)
+                    Console.WriteLine("Invalid price!");
+            }
+            while (!valid);
+
+            Meat meat = new(GetMeatId(filePath), type, price);
+
+            File.AppendAllText(filePath, meat.ToString() + Environment.NewLine);
+
+            ConsoleKeyInfo key;
 
             do
             {
-                Console.Clear();
+                Console.Write("\nAdd more one meat (y/n)? ");
+                key = Console.ReadKey(true);
 
+                if (key.KeyChar != 'y' && key.KeyChar != 'n') Console.WriteLine("\nInvalid key!");
+            }
+            while (key.KeyChar != 'y' && key.KeyChar != 'n');
+
+            continueOption = key.KeyChar == 'y';
+
+        }
+        while (continueOption);
+    }
+
+    static int GetMeatId(string file)
+    {
+        int lastId;
+
+        string[] fileLines = File.ReadAllLines(file);
+
+        if (fileLines.Length == 0)
+        {
+            lastId = 1;
+            return lastId;
+        }
+        string[] lastLine = fileLines[fileLines.Length - 1].Split(';');
+
+        lastId = int.Parse(lastLine[0]);
+
+        lastId += 1;
+        return lastId;
+    }
+
+    static void ListAllMeats(string file)
+    {
+        Console.Clear();
+
+        string[] fileLines = File.ReadAllLines(file);
+
+        Console.WriteLine("List of meats: ");
+
+        foreach (string line in fileLines)
+        {
+            Console.WriteLine(line);
+        }
+    }
+
+    static void RemoveMeat(string file)
+    {
+        Console.Clear();
+
+        ListAllMeats(file);
+
+        int id;
+        bool validId;
+
+        do
+        {
+            string input = SendMessageAndGetValue("Enter with the meat ID to be deleted: ");
+            validId = int.TryParse(input, CultureInfo.InvariantCulture, out id);
+        }
+        while (!validId);
+
+        string[] lines = File.ReadAllLines(file);
+
+        bool idExists = CheckIdExist(id, lines);
+
+        if (!idExists)
+        {
+            Console.Clear();
+            Console.WriteLine("Id not exist!");
+            return;
+        }
+
+        RemoveLine(file, lines, id);
+        Console.WriteLine("\nMeat deleted!");
+
+    }
+
+    static void RemoveLine(string file, string[] lines, int id)
+    {
+        List<string> remainingLines = [];
+
+        string firstChar;
+
+        foreach (string line in lines)
+        {
+            firstChar = line.Split(';')[0];
+
+            if (int.Parse(firstChar) != id)
+            {
+                remainingLines.Add(line);
+            }
+        }
+
+        File.WriteAllLines(file, remainingLines);
+    }
+
+    static bool CheckIdExist(int id, string[] lines)
+    {
+        string firstChar;
+
+        foreach (string line in lines)
+        {
+            firstChar = line.Split(';')[0];
+
+            if (int.Parse(firstChar) == id)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static void PressKeyMessage()
+    {
+        Console.WriteLine("\nPress any key to continue...");
+        Console.ReadKey(true);
+        Console.Clear();
+    }
+
+    static string SendMessageAndGetValue(string message)
+    {
+        Console.Write(message);
+        string input = Console.ReadLine();
+
+        return input;
+    }
+
+    static void EditMeat(string file)
+    {
+        ListAllMeats(file);
+
+        int id;
+        bool validId;
+
+        do
+        {
+            string input = SendMessageAndGetValue("\nEnter with the meat ID to be edited: ");
+
+            validId = int.TryParse(input, out id);
+
+            if (!validId)
+                Console.WriteLine("\nInvalid id!");
+        }
+        while (!validId);
+
+        string[] fileLines = File.ReadAllLines(file);
+
+        bool existId = CheckIdExist(id, fileLines);
+
+        if (!existId)
+        {
+            Console.Clear();
+            Console.WriteLine("Id do not exist!");
+            return;
+        }
+
+        string inputType = "";
+
+        List<string> list = [];
+
+        foreach (string line in fileLines)
+        {
+            string firstChar = line.Split(";")[0];
+
+            if (int.Parse(firstChar) != id)
+            {
+                list.Add(line);
+            }
+            else
+            {
                 Console.Write("Type of meat: ");
                 string type = Console.ReadLine();
 
@@ -73,145 +271,15 @@ internal class Program
                 }
                 while (!valid);
 
-                Meat meat = new(GetMeatId(filePath), type, price);
+                string editedItem = $"{id};{type};{price}";
 
-                File.AppendAllText(filePath, meat.ToString() + Environment.NewLine);
-
-                ConsoleKeyInfo key;
-
-                do
-                {
-                    Console.Write("\nAdd more one meat (y/n)? ");
-                    key = Console.ReadKey(true);
-
-                    if (key.KeyChar != 'y' && key.KeyChar != 'n') Console.WriteLine("\nInvalid key!");
-                }
-                while (key.KeyChar != 'y' && key.KeyChar != 'n');
-
-                continueOption = key.KeyChar == 'y';
-    
-            }
-            while (continueOption);
-        }
-
-        static int GetMeatId(string file)
-        {
-            int lastId;
-
-            string[] fileLines = File.ReadAllLines(file);
-
-            if (fileLines.Length == 0)
-            {
-                lastId = 1;
-                return lastId;
-            }
-            string[] lastLine = fileLines[fileLines.Length - 1].Split(';');
-
-            lastId = int.Parse(lastLine[0]);
-
-            lastId += 1;
-            return lastId;
-        }
-
-        static void ListAllMeats(string file)
-        {
-            Console.Clear();
-
-            string[] fileLines = File.ReadAllLines(file);
-
-            Console.WriteLine("List of meats: ");
-
-            foreach (string line in fileLines)
-            {
-                Console.WriteLine(line);
+                list.Add(editedItem);
             }
         }
 
-        static void RemoveMeat(string file)
-        {
-            Console.Clear();
+        File.WriteAllLines(file, list);
 
-            ListAllMeats(file);
+        Console.WriteLine("\nItem was edited!");
 
-            int id;
-            bool validId;
-
-            do
-            {
-                Console.Write("\nEnter the meat ID to be deleted: ");
-                string input = Console.ReadLine();
-
-                validId = int.TryParse(input, CultureInfo.InvariantCulture, out id);
-
-                if (!validId)
-                    Console.WriteLine("Invalid id!");
-            }
-            while (!validId);
-
-            string[] lines = File.ReadAllLines(file);
-
-            bool idExists = CheckIdExist(id, lines);
-
-            if (!idExists)
-            {
-                Console.Clear();
-                Console.WriteLine("Id not exist!");
-            }
-
-            if (idExists)
-            {
-                RemoveLine(file, lines, id);
-                Console.WriteLine("\nMeat deleted!");
-            }
-
-        }
-
-        static void RemoveLine(string file, string[] lines, int id)
-        {
-            List<string> remainingLines = [];
-
-            string firstChar;
-
-            foreach (string line in lines)
-            {
-                firstChar = line.Split(';')[0];
-
-                if (int.Parse(firstChar) != id)
-                {
-                    remainingLines.Add(line);
-                }
-            }
-
-            File.WriteAllLines(file, remainingLines);
-        }
-
-        static bool CheckIdExist(int id, string[] lines)
-        {
-            string firstChar;
-
-            foreach (string line in lines)
-            {
-                firstChar = line.Split(';')[0];
-
-                if (int.Parse(firstChar) == id)
-                {
-                    return true;
-                }   
-            }
-
-            return false;
-        }
-
-        static void PressKeyMessage()
-        {
-            Console.WriteLine("\nPress any key to continue...");
-            Console.ReadKey(true);
-            Console.Clear();
-        }
-
-        static void EditMeat()
-        {
-
-        }
     }
 }
