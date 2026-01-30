c3158a3ccb9e9d39e5d77fdb282a5a4578559b99
Guilherme Cristaldo <guilherme.cristaldo15@gmail.com>
2026-01-29 22:23:59 -0300

    Refactor: organize folders and create IMeatRepository with MeatRepository

---
+++ MeatControl.Console/MeatControl.Console/Repositories/MeatRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeatControl.Console.Repositories
+{
+    internal class MeatRepository
+    {
+    }
+}
---
+++ MeatControl.Console/MeatControl.Console/Services/MeatService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeatControl.Console.Services
+{
+    internal class MeatService
+    {
+    }
+}
